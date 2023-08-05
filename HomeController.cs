2c7d1aed359d43991ea16c753eaebb74edb9f0c0
Vaishali Parmeshwar Marsone <82966052+Vaishali-marsone@users.noreply.github.com>
2023-08-06 00:19:36 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApplication2
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Seatbooking()
+        {
+            ViewBag.shapeData = this.SeatData();
+            return View();
+        }
+        public object SeatData()
+        {
+            string allText = System.IO.File.ReadAllText(Microsoft.AspNetCore.Server.MapPath("~/App_Data/MapData/Seat.json"));
+            return JsonConvert.DeserializeObject(allText, typeof(object));
+        }
+    }
+}
